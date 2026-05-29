c6c062d50c5a3aa0c203e5f6a2ce91798f7bb6aa
Jainik Bhandari <jainikdmn@gmail.com>
2026-05-29 21:38:44 +0530

    auth done

--- server/app.js
+++ server/app.js
@@ -7,10 +7,16 @@ const app = express();
 
 connectDB();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("ResumeIQ API Running");
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+app.use("/api/auth", require("./routes/authRoutes"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
