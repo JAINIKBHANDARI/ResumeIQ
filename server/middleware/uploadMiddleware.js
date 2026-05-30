e3534044db2c8380de56af511a0643768b52f975
Jainik Bhandari <jainikdmn@gmail.com>
2026-05-30 22:47:42 +0530

    fixed resume upload

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -2,10 +2,10 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
-const uploadDir = "uploads";
+const uploadDir = path.join(__dirname, "..", "uploads");
 
 if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
+    fs.mkdirSync(uploadDir, { recursive: true });
 }
 
 const storage = multer.diskStorage({
